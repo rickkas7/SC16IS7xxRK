061f8c7d1aad92e268240a047a72acf7d36bc4d0
rickkas7 <rickk@rickk.com>
2023-08-07 08:38:20 -0400

    checkpoint

--- src/SC16IS7xxRK.cpp
+++ src/SC16IS7xxRK.cpp
@@ -1,5 +1,133 @@
 #include "SC16IS7xxRK.h"
 
+static Logger _uartLogger = Logger("app.uart");
+
+int SC16IS7xxPort::available() {
+	return interface->readRegister(channel, SC16IS7xxInterface::RXLVL_REG);
+}
+
+int SC16IS7xxPort::availableForWrite() {
+	return interface->readRegister(channel, SC16IS7xxInterface::TXLVL_REG);
+}
+
+
+int SC16IS7xxPort::read() {
+	if (hasPeek) {
+		hasPeek = false;
+		return peekByte;
+	}
+	else {
+		if (available()) {
+			return interface->readRegister(channel, SC16IS7xxInterface::RHR_THR_REG);
+		}
+		else {
+			return -1;
+		}
+	}
+}
+
+int SC16IS7xxPort::peek() {
+	if (!hasPeek) {
+		peekByte = read();
+		hasPeek = true;
+	}
+	return peekByte;
+}
+
+void SC16IS7xxPort::flush() {
+	while(availableForWrite() < 64) {
+		delay(1);
+	}
+}
+
+size_t SC16IS7xxPort::write(uint8_t c) {
+
+	if (writeBlocksWhenFull) {
+		// Block until there is room in the buffer
+		while(!availableForWrite()) {
+			delay(1);
+		}
+	}
+
+	interface->writeRegister(channel, SC16IS7xxInterface::RHR_THR_REG, c);
+
+	return 1;
+}
+
+size_t SC16IS7xxPort::write(const uint8_t *buffer, size_t size) {
+	size_t written = 0;
+	bool done = false;
+
+	while(size > 0 && !done) {
+		size_t count = size;
+		if (count > interface->writeInternalMax()) {
+			count = interface->writeInternalMax();
+		}
+
+		if (writeBlocksWhenFull) {
+			while(true) {
+				int avail = availableForWrite();
+				if (count <= (size_t) avail) {
+					break;
+				}
+				delay(1);
+			}
+		}
+		else {
+			int avail = availableForWrite();
+			if (count > (size_t) avail) {
+				count = (size_t) avail;
+				done = true;
+			}
+		}
+
+		if (count == 0) {
+			break;
+		}
+
+		if (!interface->writeInternal(channel, buffer, count)) {
+			// Failed to write
+			break;
+		}
+		buffer += count;
+		size -= count;
+		written += count;
+	}
+
+	return written;
+}
+
+/**
+ * @brief Read a multiple bytes to the serial port.
+ *
+ * @param buffer The buffer to read data into. It will not be null terminated.
+ *
+ * @param size The maximum number of bytes to read (buffer size)
+ *
+ * @return The number of bytes actually read or -1 if there are no bytes available to read.
+ *
+ * This is faster than reading a single byte at time because up to 32 bytes of data can
+ * be sent or received in an I2C transaction, greatly reducing overhead.
+ */
+int SC16IS7xxPort::read(uint8_t *buffer, size_t size) {
+	int avail = available();
+	if (avail == 0) {
+		// No data to read
+		return -1;
+	}
+	if (size > (size_t) avail) {
+		size = (size_t) avail;
+	}
+	if (size > interface->readInternalMax()) {
+		size = interface->readInternalMax();
+	}
+	if (!interface->readInternal(channel, buffer, size)) {
+		return -1;
+	}
+
+	return (int) size;
+}
+
 
 
 SC16IS7xxInterface &SC16IS7xxInterface::withI2C(TwoWire *wire, uint8_t addr) {
@@ -50,23 +178,188 @@ void SC16IS7xxInterface::endTransaction() {
 
 
 
-protected:
-    SC16IS7xxInterface() {};
-    virtual ~SC16IS7xxInterface() {};
 
-    /**
-     * @brief This class is not copyable
-     */
-    SC16IS7xxInterface(const SC16IS7xxInterface&) = delete;
+// Note: reg is the register 0 - 15, not the shifted value with the channel select bits. Channel is always 0
+// on the SC16IS7xxInterface.
+uint8_t SC16IS7xxInterface::readRegister(uint8_t channel, uint8_t reg) {
+    uint8_t value = 0;
 
-    /**
-     * @brief This class is not copyable
-     */
-    SC16IS7xxInterface& operator=(const SC16IS7xxInterface&) = delete;
+    beginTransaction();
+
+    if (spi) {
+        spi->transfer(0x80 | reg << 3 | channel << 1);
+        value = (uint8_t) spi->transfer(0);
+
+        endTransaction();
+    }
+    else
+    if (wire) {
+
+        wire->beginTransmission(i2cAddr);
+        wire->write(reg << 3 | channel << 1);
+        wire->endTransmission(false);
+
+        wire->requestFrom(i2cAddr, 1, true);
+        value = (uint8_t) wire->read();
+    }
+
+	_uartLogger.trace("readRegister chan=%d reg=%d value=%d", channel, reg, value);
+
+    endTransaction();
+
+	return value;
+}
+
+// Note: reg is the register 0 - 15, not the shifted value with the channel select bits
+bool SC16IS7xxInterface::writeRegister(uint8_t channel, uint8_t reg, uint8_t value) {
+    bool result = false;
+
+    beginTransaction();
+
+    if (spi) {
+        spi->transfer(reg << 3 | channel << 1);
+        spi->transfer(value);
+
+        _uartLogger.trace("writeRegister chan=%d reg=%d value=%d", channel, reg, value);
+    }
+    else
+    if (wire) {
+        wire->beginTransmission(i2cAddr);
+        wire->write(reg << 3 | channel << 1);
+        wire->write(value);
+
+        int stat = wire->endTransmission(true);
+
+        // stat:
+        // 0: success
+        // 1: busy timeout upon entering endTransmission()
+        // 2: START bit generation timeout
+        // 3: end of address transmission timeout
+        // 4: data byte transfer timeout
+        // 5: data byte transfer succeeded, busy timeout immediately after
+
+        _uartLogger.trace("writeRegister chan=%d reg=%d value=%d stat=%d", channel, reg, value, stat);
+
+        result = (stat == 0);
+    }
+
+    endTransaction();
+
+	return result;
+}
 
 
-    TwoWire *wire = nullptr;
-    uint8_t i2cAddr = 0;
-    SPIClass *spi = nullptr;
-    pin_t csPin;
-    SPISettings spiSettings;
+bool SC16IS7xxInterface::readInternal(uint8_t channel, uint8_t *buffer, size_t size) {
+    bool result = false;
+
+    beginTransaction();
+
+    if (spi) {
+    	spi->transfer(NULL, buffer, size, NULL);
+    }
+    else
+    if (wire) {
+        wire->beginTransmission(i2cAddr);
+        wire->write(RHR_THR_REG << 3 | channel << 1);
+        wire->endTransmission(false);
+
+        uint8_t numRcvd = wire->requestFrom(i2cAddr, size, (uint8_t)true);
+        if (numRcvd == size) {
+            for(size_t ii = 0; ii < size; ii++) {
+                buffer[ii] = (uint8_t) wire->read();
+            }
+
+            _uartLogger.trace("readInternal %d bytes", size);
+
+            result = true;
+        }
+        if (numRcvd < size) {
+            _uartLogger.info("readInternal failed numRcvd=%u size=%u", numRcvd, size);
+        }
+    }
+
+
+    endTransaction();
+
+	return result;
+}
+
+
+bool SC16IS7xxInterface::writeInternal(uint8_t channel, const uint8_t *buffer, size_t size) {
+    bool result = true;
+
+    beginTransaction();
+
+    if (spi) {
+        spi->transfer(RHR_THR_REG << 3 | channel << 1);
+    	spi->transfer(const_cast<uint8_t *>(buffer), NULL, size, NULL);
+
+        _uartLogger.trace("writeInternal size=%d", size);
+    }
+    else
+    if (wire) {
+        wire->beginTransmission(i2cAddr);
+        wire->write(RHR_THR_REG << 3 | channel << 1);
+        wire->write(buffer, size);
+
+        int stat = wire->endTransmission(true);
+
+        // stat:
+        // 0: success
+        // 1: busy timeout upon entering endTransmission()
+        // 2: START bit generation timeout
+        // 3: end of address transmission timeout
+        // 4: data byte transfer timeout
+        // 5: data byte transfer succeeded, busy timeout immediately after
+
+        _uartLogger.trace("writeInternal size=%d stat=%d", size, stat);
+
+        result = (stat == 0);
+    }
+
+	return result;
+}
+
+size_t SC16IS7xxInterface::readInternalMax() const {
+    size_t value = 0;
+
+    if (spi) {
+        // SPI does allow a large value here, but the FIFO is limited to this size.
+        value = 64;
+    }
+    else
+    if (wire) {
+        // Newer version of Device OS can change the size of the I2C buffer on some
+        // platforms, but this should be big enough for most I2C use cases.
+        value = 32;
+    }
+    return value;
+}
+
+size_t SC16IS7xxInterface::writeInternalMax() const {
+    size_t value = 0;
+
+    if (spi) {
+        // SPI does allow a large value here, but the FIFO is limited to this size.
+        value = 64;
+    }
+    else
+    if (wire) {
+        // Newer version of Device OS can change the size of the I2C buffer on some
+        // platforms, but this should be big enough for most I2C use cases.
+        // Note that this is not 32!
+        value = 31;
+    }
+    return value;
+}
+
+SC16IS7x0::SC16IS7x0() {
+    interface = this;
+}
+
+SC16IS7x2::SC16IS7x2() {
+    for(size_t ii = 0; ii < (sizeof(ports) / sizeof(ports[0])); ii++) {
+        ports[ii].channel = ii;
+        ports[ii].interface = this;
+    }
+}
--- src/SC16IS7xxRK.h
+++ src/SC16IS7xxRK.h
@@ -4,6 +4,7 @@
 #include "Particle.h"
 
 
+class SC16IS7xxInterface; // Forward declaration
 class SC16IS7x2; // Forward declaration
 
 /**
@@ -14,6 +15,130 @@ class SC16IS7x2; // Forward declaration
  */
 class SC16IS7xxPort {
 public:
+	/**
+	 * @brief Set up the chip. You must do this before reading or writing.
+	 *
+	 * @param baudRate the baud rate (see below)
+	 *
+	 * @param options The number of data bits, parity, and stop bits
+	 *
+	 * You can call begin more than once if you want to change the baud rate. The FIFOs are
+	 * cleared when you call begin.
+	 *
+	 * Available baud rates depend on your oscillator, but with a 1.8432 MHz oscillator, the following are supported:
+	 * 50, 75, 110, 134.5, 150, 300, 600, 1200, 1800, 2000, 2400, 3600, 4800, 7200, 9600, 19200, 38400, 57600, 115200
+	 *
+	 * The valid options in standard number of bits; none=N, even=E, odd=O; number of stop bits format:
+	 * OPTIONS_8N1, OPTIONS_8E1, OPTIONS_8O1
+	 * OPTIONS_8N2, OPTIONS_8E2, OPTIONS_8O2
+	 * OPTIONS_7N1, OPTIONS_7E1, OPTIONS_7O1
+	 * OPTIONS_7N2, OPTIONS_7E2, OPTIONS_7O2
+	 */
+	bool begin(int baudRate, uint8_t options = OPTIONS_8N1);
+
+	/**
+	 * @brief Defines what should happen when calls to write()/print()/println()/printlnf() that would overrun the buffer.
+	 *
+	 * blockOnOverrun(true) - this is the default setting. When there is no room in the buffer for the data to be written,
+	 * the program waits/blocks until there is room. This avoids buffer overrun, where data that has not yet been sent over
+	 * serial is overwritten by new data. Use this option for increased data integrity at the cost of slowing down realtime
+	 * code execution when lots of serial data is sent at once.
+	 *
+	 * blockOnOverrun(false) - when there is no room in the buffer for data to be written, the data is written anyway,
+	 * causing the new data to replace the old data. This option is provided when performance is more important than data
+	 * integrity.
+	 */
+	inline void blockOnOverrun(bool value = true) { writeBlocksWhenFull = value; };
+
+	/**
+	 * @brief Returns the number of bytes available to read from the serial port
+	 *
+	 * This is a standard Arduino/Wiring method for Stream objects.
+	 */
+    virtual int available();
+
+	/**
+	 * @brief Returns the number of bytes available to write into the TX FIFO
+	 */
+    virtual int availableForWrite();
+
+	/**
+	 * @brief Read a single byte from the serial port
+	 *
+	 * @return a byte value 0 - 255 or -1 if no data is available.
+	 *
+	 * This is a standard Arduino/Wiring method for Stream objects.
+	 */
+    virtual int read();
+
+	/**
+	 * @brief Read a single byte from the serial port, but do not remove it so it can be read again.
+	 *
+	 * @return a byte value 0 - 255 or -1 if no data is available.
+	 *
+	 * This is a standard Arduino/Wiring method for Stream objects.
+	 */
+    virtual int peek();
+
+	/**
+	 * @brief Block until all serial data is sent.
+	 *
+	 * This is a standard Arduino/Wiring method for Stream objects.
+	 */
+    virtual void flush();
+
+	/**
+	 * @brief Write a single byte to the serial port.
+	 *
+	 * @param c The byte to write. Can write binary or text data.
+	 *
+	 * This is a standard Arduino/Wiring method for Stream objects.
+	 */
+    virtual size_t write(uint8_t c);
+
+	/**
+	 * @brief Write a multiple bytes to the serial port.
+	 *
+	 * @param buffer The buffer to write. Can write binary or text data.
+	 *
+	 * @param size The number of bytes to write
+	 *
+	 * @return The number of bytes written.
+	 *
+	 * This is faster than writing a single byte at time because up to 32 bytes of data can
+	 * be sent or received in an I2C transaction, greatly reducing overhead.
+	 */
+	virtual size_t write(const uint8_t *buffer, size_t size);
+
+	/**
+	 * @brief Read a multiple bytes to the serial port.
+	 *
+	 * @param buffer The buffer to read data into. It will not be null terminated.
+	 *
+	 * @param size The maximum number of bytes to read (buffer size)
+	 *
+	 * @return The number of bytes actually read or -1 if there are no bytes available to read.
+	 *
+	 * This is faster than reading a single byte at time because up to 32 bytes of data can
+	 * be sent or received in an I2C transaction, greatly reducing overhead.
+	 */
+	virtual int read(uint8_t *buffer, size_t size);
+
+	static const uint8_t OPTIONS_8N1 = 0b000011;
+	static const uint8_t OPTIONS_8E1 = 0b011011;
+	static const uint8_t OPTIONS_8O1 = 0b001011;
+
+	static const uint8_t OPTIONS_8N2 = 0b000111;
+	static const uint8_t OPTIONS_8E2 = 0b011111;
+	static const uint8_t OPTIONS_8O2 = 0b001111;
+
+	static const uint8_t OPTIONS_7N1 = 0b000010;
+	static const uint8_t OPTIONS_7E1 = 0b011010;
+	static const uint8_t OPTIONS_7O1 = 0b001010;
+
+	static const uint8_t OPTIONS_7N2 = 0b000110;
+	static const uint8_t OPTIONS_7E2 = 0b011110;
+	static const uint8_t OPTIONS_7O2 = 0b001110;
 
 protected:
     SC16IS7xxPort() {};
@@ -29,6 +154,12 @@ protected:
      */
     SC16IS7xxPort& operator=(const SC16IS7xxPort&) = delete;
 
+	bool hasPeek = false;
+	uint8_t peekByte = 0;
+	bool writeBlocksWhenFull = true;
+    uint8_t channel = 0;
+    SC16IS7xxInterface *interface = nullptr;
+
     friend class SC16IS7x2;
 };
 
@@ -39,6 +170,7 @@ protected:
  */
 class SC16IS7xxInterface {
 public:
+
     /**
      * @brief Chip is connected by I2C
      * 
@@ -81,10 +213,61 @@ public:
      */
     SC16IS7xxInterface &withSPI(SPIClass *spi, pin_t csPin, size_t speedMHz);
 
-    void beginTransaction();
+    /**
+     * @brief Set the oscillator frequency for the chip
+     * 
+     * @param freqHz Typically 1843200 (default, 1.8432 MHz), or 3072000 (3.072 MHz).
+     * @return * SC16IS7xxInterface& 
+     * 
+     */
+    SC16IS7xxInterface &withOscillatorFrequency(int freqHz) { this->oscillatorFreqHz = freqHz; return *this; };
 
-    void endTransaction();
 
+    /**
+     * @brief Read a register
+     *
+     * @param reg The register number to read. Note that this should be the register 0 - 16, before shifting for channel.
+     */
+	uint8_t readRegister(uint8_t channel, uint8_t reg);
+
+	/**
+     * @brief Write a register
+     *
+     * @param reg The register number to write. Note that this should be the register 0 - 16, before shifting for channel.
+     *
+     * @param value The value to write
+     */
+	bool writeRegister(uint8_t channel, uint8_t reg, uint8_t value);
+
+
+	static const uint8_t RHR_THR_REG = 0x00;
+	static const uint8_t IEF_REG = 0x01;
+	static const uint8_t FCR_IIR_REG = 0x02;
+	static const uint8_t LCR_REG = 0x03;
+	static const uint8_t MCR_REG = 0x04;
+	static const uint8_t LSR_REG = 0x05;
+	static const uint8_t MSR_REG = 0x06;
+	static const uint8_t SPR_REG = 0x07;
+	static const uint8_t TXLVL_REG = 0x08;
+	static const uint8_t RXLVL_REG = 0x09;
+	static const uint8_t IODIR_REG = 0x0a;
+	static const uint8_t IOSTATE_REG = 0x0b;
+	static const uint8_t IOINTENA_REG = 0x0c;
+	static const uint8_t IOCONTROL_REG = 0x0e;
+	static const uint8_t EFCR_REG = 0x0f;
+
+	// Special register block
+	static const uint8_t LCR_SPECIAL_START = 0x80;
+	static const uint8_t LCR_SPECIAL_END = 0xbf;
+	static const uint8_t DLL_REG = 0x00;
+	static const uint8_t DLH_REG = 0x01;
+
+	// Enhanced register set
+	static const uint8_t EFR_REG = 0x02;
+	static const uint8_t XON1_REG = 0x04;
+	static const uint8_t XON2_REG = 0x05;
+	static const uint8_t XOFF1_REG = 0x06;
+	static const uint8_t XOFF2_REG = 0x07;
 
 protected:
     SC16IS7xxInterface() {};
@@ -100,12 +283,60 @@ protected:
      */
     SC16IS7xxInterface& operator=(const SC16IS7xxInterface&) = delete;
 
+    /**
+     * @brief Begins a SPI or I2C transaction
+     * 
+     * A transaction is a group of related calls. Within the transaction other devices are prohibited
+     * from accessing the bus, but thread swapping is still enabled.
+     */
+    void beginTransaction();
+
+    /**
+     * @brief Ends a SPI or I2C transaction
+     * 
+     * A transaction is a group of related calls. Within the transaction other devices are prohibited
+     * from accessing the bus, but thread swapping is still enabled.
+     */
+    void endTransaction();
+
+
+	/**
+	 * @brief Internal function to read data
+	 *
+	 * It can only read 32 bytes at a time, the maximum that will fit in a 32 byte I2C transaction with
+	 * the register address in the first byte.
+	 */
+	bool readInternal(uint8_t channel, uint8_t *buffer, size_t size);
+
+
+	/**
+	 * @brief Internal function to write data
+	 *
+	 * It can only write 31 bytes at a time, the maximum that will fit in a 32 byte I2C transaction with
+	 * the register address in the first byte.
+	 */
+	virtual bool writeInternal(uint8_t channel, const uint8_t *buffer, size_t size);
+
+	/**
+	 * @brief Maximum number of bytes that can be read by readInternal
+	 */
+	size_t readInternalMax() const;
+
+	/**
+	 * @brief Maximum number of bytes that can be written by writeInternal
+	 */
+	size_t writeInternalMax() const;
+
+
 
     TwoWire *wire = nullptr;
     uint8_t i2cAddr = 0;
     SPIClass *spi = nullptr;
     pin_t csPin;
     SPISettings spiSettings;
+    int oscillatorFreqHz = 1843200; // 1.8432 MHz
+
+    friend class SC16IS7xxPort;
 
 };
 
@@ -120,7 +351,7 @@ public:
      * 
      * You typically allocate one of these per 
      */
-    SC16IS7x0() {};
+    SC16IS7x0();
     virtual ~SC16IS7x0() {};
 
     /**
@@ -173,7 +404,7 @@ public:
      * 
      * You typically allocate one of these per 
      */
-    SC16IS7x2() {};
+    SC16IS7x2();
     virtual ~SC16IS7x2() {};
 
     /**
